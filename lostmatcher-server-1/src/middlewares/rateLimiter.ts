65ad69bf1b7c1b53fd2a5b1b5e563bb57f183982
Kouete-kenneth <kouete678@gmail.com>
2025-06-18 22:13:19 +0100

    First up and running version of the LostMatcher

--- lostmatcher-server-1/src/middlewares/rateLimiter.ts
+++ lostmatcher-server-1/src/middlewares/rateLimiter.ts
@@ -1,1 +1,29 @@
-npm install --save-dev typescript @types/node
+import { Request, Response, NextFunction } from 'express';
+
+interface RateLimitOptions {
+    windowMs: number; // time window in ms
+    max: number; // max requests per window per IP
+}
+
+const rateLimiters: Record<string, { count: number; expires: number }> = {};
+
+const rateLimiter = (options: RateLimitOptions) => {
+    return (req: Request, res: Response, next: NextFunction) => {
+        const ip = req.ip || '';
+        const now = Date.now();
+
+        if (!rateLimiters[ip] || rateLimiters[ip].expires < now) {
+            rateLimiters[ip] = { count: 1, expires: now + options.windowMs };
+        } else {
+            rateLimiters[ip].count += 1;
+        }
+
+        if (rateLimiters[ip].count > options.max) {
+            res.status(429).json({ message: 'Too many requests, please try again later.' });
+        } else {
+            next();
+        }
+    };
+};
+
+export default rateLimiter;
--- lostmatcher-server-1/src/utils/pick.ts
+++ lostmatcher-server-1/src/utils/pick.ts
@@ -1,1 +1,16 @@
-npm install --save-dev typescript @types/node
+/**
+ * Create an object composed of the picked object properties
+ * @param {Object} object - The source object from which properties will be picked
+ * @param {string[]} keys - The keys of the properties to pick from the object
+ * @returns {Object} - A new object containing only the picked properties
+ */
+const pick = <T extends object>(object: T, keys: (keyof T)[]): Partial<T> => {
+    return keys.reduce((obj, key) => {
+        if (object && Object.prototype.hasOwnProperty.call(object, key)) {
+            obj[key] = object[key];
+        }
+        return obj;
+    }, {} as Partial<T>);
+};
+
+export default pick;

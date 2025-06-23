fcd636ceab5f887903a2762de85348945fd9693d
Kouete-kenneth <kouete678@gmail.com>
2025-06-24 00:27:54 +0100

    Frontend deleted and started from scratch

--- lostmatcher-client/utils/backendBaseURL.jsx
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-// Create an instance of Axios with a base URL
-const backendBaseURL = axios.create({
-  baseURL: 'https://finditserver.vercel.app/v1', //base url for backend
-});
-
-export default backendBaseURL;
